fede1a3200b6b76e3ec41556678b372877f5814e
Armando <armandoandrecalderon@gmail.com>
2025-09-14 16:27:45 -0400

    Looping a triangle finished

---
+++ chapter-2-program-structure/looping-a-triangle.js
@@ -0,0 +1,10 @@
+// Looping a Triangle
+// Instructions: Write a loop that makes seven calls to console.log to output the following triangle.
+
+let output = "";
+
+for (let counter = 0; counter < 7; counter++) {
+  output += "#";
+
+  console.log(output);
+}
