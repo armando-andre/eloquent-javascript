90d91f25a8c940c8225374eafc5d5d3267133f71
Armando <armandoandrecalderon@gmail.com>
2025-09-15 11:04:18 -0400

    Minimum finished

---
+++ chapter-3-functions/minimum.js
@@ -0,0 +1,4 @@
+// Minimum
+// Instructions: Define the function min that takes two arguments and returns their minimum.
+
+let min = (firstNumber, secondNumber) => Math.min(firstNumber, secondNumber);
