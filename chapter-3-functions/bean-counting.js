1300bd2306650fa5ab0e45f03d34db800f189fbe
Armando <armandoandrecalderon@gmail.com>
2025-09-15 13:11:53 -0400

    Bean Counting finished

--- chapter-3-functions/bean-counting.js
+++ chapter-3-functions/bean-counting.js
@@ -3,3 +3,19 @@
 
 // Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase B characters).
 // Rewrite countBs to make use of this new function.
+
+const countChar = (userInput, letterToLookFor) => {
+  let charactersCounter = 0;
+
+  for (let i = 0; i < userInput.length; i++) {
+    if (userInput[i] === letterToLookFor) {
+      charactersCounter++;
+    }
+  }
+
+  return charactersCounter;
+};
+
+const countBs = (userInput) => {
+  return countChar(userInput, "B");
+};
